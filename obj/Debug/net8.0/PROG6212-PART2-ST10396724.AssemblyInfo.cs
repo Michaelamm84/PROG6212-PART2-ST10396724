8dcc2a4fc6fabdaf3ddb1f1790bdc870ab06ca0a
Michaelamm84 <ST10396724@vcconnect.edu.za>
2024-10-18 21:49:57 +0100

    adding Unit Testing for methods in operations class

--- obj/Debug/net8.0/PROG6212-PART2-ST10396724.AssemblyInfo.cs
+++ obj/Debug/net8.0/PROG6212-PART2-ST10396724.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PROG6212-PART2-ST10396724")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1da6bdc31672aec039716925ef4b3af6e038837")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e56ff7f57a3844db2161852092825202c453fc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("PROG6212-PART2-ST10396724")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PROG6212-PART2-ST10396724")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
